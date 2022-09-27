5e5ecf09887e499ef3b6a1d8f22e42ed944243d8
Fabricio-Justino <fabriciobj667@gmail.com>
2022-09-27 16:30:56 -0300

    craeting function to draw the array

--- js/main.js
+++ js/main.js
@@ -11,8 +11,9 @@ $body.appendChild($canvas);
 const rectY = h/2 - 10;
 const rectW = 600 / 10;
 const rectH = 20;
+const m = 20; // margin
 
-let board = [];
+let board = [1, 2, 3, 4];
 
 cls();
 function cls() {
@@ -29,3 +30,26 @@ function generateNumbers(arrLength, k, a, b, c) {
     return ar;
 }
 
+function drawArray(arr, currentPos) {
+    const value = arr[currentPos];
+
+    ctx.beginPath();
+    ctx.font = 'serif 12px';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+
+    ctx.strokeStyle = 'white';
+    arr.forEach((el, i) => {
+        if (i === currentPos) {
+            ctx.fillStyle = 'blue';
+            ctx.fillRect(m + i * rectW,  rectY, rectW, rectH);
+            ctx.strokeRect(m + i * rectW,  rectY, rectW, rectH);
+        } else {
+            ctx.strokeRect(m + i * rectW,  rectY, rectW, rectH);
+        }
+        ctx.strokeText(el, m + i * rectW + (rectW / 2), rectY + (rectH / 2) );
+    });
+    ctx.closePath();
+}
+
+drawArray(board, 0);
